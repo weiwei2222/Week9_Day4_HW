a5736a874e6889bd4efb929e247ced7bda773148
Weiwei <linweiwei2222@163.com>
2023-11-17 18:48:58 -0500

    homework

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -3,13 +3,13 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Stock(props) {
-  const apiKey = "";
+  const apiKey = "30d53e20d2075e17a1d54260b6dc8226";
   const params = useParams();
   const symbol = params.symbol;
   const url = `https://financialmodelingprep.com/api/v3/quote/${symbol}?apikey=${apiKey}`;
   const [stock, setStock] = useState(null);
 
-  const getCoin = async () => {
+  const getStock = async () => {
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -21,7 +21,7 @@ function Stock(props) {
 
   // useEffect to run getCoin when component mounts
   useEffect(() => {
-    getCoin();
+    getStock();
   }, []);
 
   const loaded = () => {
